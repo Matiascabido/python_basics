d1671a0c530a89ab30a80205285810e495c0a000
Matiascabido <cabidomatias@gmail.com>
2022-12-09 22:12:18 -0300

    first clases.

---
+++ 00_hello_world.py
@@ -0,0 +1,8 @@
+# Esto es un comentario: 
+# Es un hola mundo en python.
+print("Hello Word")
+print('Hello Python')
+
+"""
+  Este es una forma de comentario en varias lineas
+"""
---
+++ 01_variables.py
@@ -0,0 +1,20 @@
+"""
+  Tipos de datos 
+"""
+
+print(type("Hello Word")) # Tipo str
+print(type(5)) # Tipo int
+print(type(5.5)) # Tipo float
+print(type(5 + 1j)) # Tipo complejo
+print(type(True)) # Tipo bool
+print(type([])) # Tipo list
+
+"""
+  Variasbles
+"""
+
+
+miVariable = 'Mi variable string' # Esto no es una forma correcta de nombrar una variabl
+mi_variable = 'Mi stirng variable' # Esta es una forma correcta.
+
+
